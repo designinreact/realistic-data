450c5dfa56e275f9a93ead728dfa92812c5fe0b4
Learning D3 <learningd3js@gmail.com>
2017-06-14 21:57:41 -0700

    Finished card example

--- src/App.js
+++ src/App.js
@@ -3,19 +3,38 @@ import Card from './Card';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/?results=30").then(function(response) {
+      response.json().then(function(json) {
+        this.setState({
+          users: json.results
+        })
+      }.bind(this))
+    }.bind(this));
+  }
+
   render() {
     return (
       <div className="App">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {this.state.users.map(function(user, i) {
+          return (
+            <Card
+              key={i}
+              avatar={user.picture.large}
+              name={user.name.first}
+              followers={Math.round(Math.random() * 300)}
+            />
+          )
+        })}
+
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -4,30 +4,30 @@ import './card.css'
 class Card extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      name: ""
+      followed: false
     }
+    this.toggleFollowed = this.toggleFollowed.bind(this);
   }
 
-  componentDidMount() {
-    fetch("https://randomuser.me/api/?results=50").then(function(response) {
-      response.json().then(function(json) {
-        console.log(json)
-        this.setState({
-          name: json.results[0].name.first
-        })
-      }.bind(this))
-    }.bind(this));
+  toggleFollowed() {
+    this.setState({
+      followed: !this.state.followed
+    })
   }
 
   render() {
     return (
       <div className="card-container">
-        <img className="card-avatar" src="" />
-        <h1>{this.state.name}</h1>
+        <img className="card-avatar" src={this.props.avatar} />
+        <div className="card-title">{this.props.name}</div>
+        <div className="card-followers">{this.props.followers} Followers</div>
+        <button onClick={this.toggleFollowed} className={`card-button ${this.state.followed ? ' card-button-active' : ''}`}>{this.state.followed ? 'Followed' : 'Follow'}</button>
       </div>
     );
   }
+
 }
 
 export default Card;
